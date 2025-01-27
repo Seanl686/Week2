5b83de3b3f92238a5abd338faa000dda3965cd02
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-01-27 17:56:01 -0500

    Added imputs on SubmitKeyForm. Non functioning.

--- src/components/SubmitPassKeyForm.jsx
+++ src/components/SubmitPassKeyForm.jsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 
-
-/**
- * // TODO: Please create an HTML form component which will take in the passkey and submit it using the function below!
- */
 export const SubmitPasskeyForm = () => {
-
-    const [cptUsername,setCptUsername] = React.useState('')
-    const [passkey,setPasskey] = React.useState('')
-    const [classCode,setClassCode] = React.useState('')
+    const [cptUsername, setCptUsername] = React.useState('');
+    const [passkey, setPasskey] = React.useState('');
+    const [classCode, setClassCode] = React.useState('');
 
     const submitPasskey = () => {
-
-        fetch('http://192.168.2.51:3000/week_two',{
+        fetch('http://192.168.2.51:3000/week_two', {
             method: 'POST',
             body: JSON.stringify({
                 'cpt_username': cptUsername,
@@ -20,31 +14,33 @@ export const SubmitPasskeyForm = () => {
                 'class_code': classCode
             }),
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
             }
         })
         .then((res) => {
-
-            if (res.status == 200) {
-                alert('Code submitted - you are done!')
+            if (res.status === 200) {
+                alert('Code submitted - you are done!');
             } else {
-                alert('Try again!')
+                alert('Try again!');
             }
-
         })
         .catch((err) => {
-
-        })
-    }
-
+            console.error(err);
+        });
+    };
 
     return (
         <div>
             <b>Please enter the username: </b>
             <input type="text" value={cptUsername} onChange={(e) => setCptUsername(e.target.value)} />
-            <button onclick={() => submitPasskey()}>Submit</button>
+            <br/>
+            <b>Please enter the class code: </b>
+            <input type="text" value={classCode} onChange={(e) => setClassCode(e.target.value)} />
+            <br/>
+            <b>Please enter the passkey: </b>
+            <input type="password" value={passkey} onChange={(e) => setPasskey(e.target.value)} />
+            <br/>
+            <button onClick={() => submitPasskey()}>Submit</button>
         </div>
-    )
-
-
-}
+    );
+};
